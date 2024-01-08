6210fab8c05e9ae3ab7d7e797feb18644fb09268
Brice Pellé <onlybakam@gmail.com>
2024-01-08 08:03:31 -0800

    --wip-- [skip ci]

--- lib/index.ts
+++ lib/index.ts
@@ -1,21 +1,269 @@
 // import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Construct } from 'constructs'
+import { findDefiningFile } from './utils'
+import path = require('path')
+import fs = require('fs')
+import {
+  AppsyncFunction,
+  AuthorizationConfig,
+  BaseDataSource,
+  CfnResolver,
+  Code,
+  DataSourceOptions,
+  Definition,
+  DomainOptions,
+  FunctionRuntime,
+  GraphqlApi,
+  HttpDataSource,
+  HttpDataSourceOptions,
+  IntrospectionConfig,
+  LogConfig,
+  NoneDataSource,
+  Resolver,
+  Visibility,
+} from 'aws-cdk-lib/aws-appsync'
 
+const DEFAULT_PIPELINE_RESOLVER_CODE = `
+export function request(ctx) { return {} }
+export function response(ctx) { return ctx.prev.result }
+
+`.trim()
 export interface AppsyncApiRouterProps {
-  // Define construct properties here
+  /**
+   * the directory to load resources from
+   */
+  basedir?: string
+
+  /**
+   * the name of the GraphQL API
+   */
+  readonly name?: string
+
+  /**
+   * Optional authorization configuration
+   *
+   * @default - API Key authorization
+   */
+  readonly authorizationConfig?: AuthorizationConfig
+
+  /**
+   * Logging configuration for this api
+   *
+   * @default - None
+   */
+  readonly logConfig?: LogConfig
+
+  /**
+   * A flag indicating whether or not X-Ray tracing is enabled for the GraphQL API.
+   *
+   * @default - false
+   */
+  readonly xrayEnabled?: boolean
+
+  /**
+   * A value indicating whether the API is accessible from anywhere (GLOBAL) or can only be access from a VPC (PRIVATE).
+   *
+   * @default - GLOBAL
+   */
+  readonly visibility?: Visibility
+
+  /**
+   * The domain name configuration for the GraphQL API
+   *
+   * The Route 53 hosted zone and CName DNS record must be configured in addition to this setting to
+   * enable custom domain URL
+   *
+   * @default - no domain name
+   */
+  readonly domainName?: DomainOptions
+
+  /**
+   * A value indicating whether the API to enable (ENABLED) or disable (DISABLED) introspection.
+   *
+   * @default IntrospectionConfig.ENABLED
+   */
+  readonly introspectionConfig?: IntrospectionConfig
 }
 
-export class AppsyncApiRouter extends Construct {
+export class AppsyncApiRouter extends GraphqlApi {
+  //Construct {
+  // readonly api: GraphqlApi
+  private base: string
+  private pipelineResolvers: Record<
+    string,
+    {
+      resolver: Resolver
+      fns: { order: number; fn: AppsyncFunction }[]
+    }
+  > = {}
+  private unitResolvers: Record<string, Resolver> = {}
+  private resolverEntries: fs.Dirent[]
 
   constructor(scope: Construct, id: string, props: AppsyncApiRouterProps = {}) {
-    super(scope, id);
+    const { name, basedir, ...restOfProps } = props
+    const apiName = props.name ?? id
+    const base = props.basedir ?? path.join(path.dirname(findDefiningFile('AppsyncApiRouter')), id)
+    super(scope, id, {
+      name: apiName,
+      definition: Definition.fromFile(path.join(base, 'schema.graphql')),
+      ...restOfProps,
+    })
+    this.base = base
+    this.loadResolverDirectory()
 
-    // Define construct contents here
+    // this.api = new GraphqlApi(this, 'appsync-graphql-api', {
+    //   name: apiName,
+    //   definition: Definition.fromFile(path.join(base, 'schema.graphql')),
+    //   ...restOfProps,
+    // })
+  }
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AppsyncApiRouterQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+  /**
+   * add a new NONE data source and load its resolvers/functions.
+   *
+   * @param id The data source's id
+   * @param options The optional configuration for this data source
+   */
+  public addNoneDataSource(id: string, options?: DataSourceOptions): NoneDataSource {
+    const ds = super.addNoneDataSource(id, options)
+    this.loadResolvers(ds)
+    return ds
+  }
+
+  /**
+   * add a new http data source to this API and loads its resolvers/functions
+   *
+   * @param id The data source's id
+   * @param endpoint The http endpoint
+   * @param options The optional configuration for this data source
+   */
+  public addHttpDataSource(
+    id: string,
+    endpoint: string,
+    options?: HttpDataSourceOptions
+  ): HttpDataSource {
+    const ds = super.addHttpDataSource(id, endpoint, options)
+    this.loadResolvers(ds)
+    return ds
+  }
+
+  private loadResolverDirectory() {
+    const folder = path.join(this.base, 'resolvers')
+    this.resolverEntries = fs.readdirSync(folder, { withFileTypes: true, recursive: true })
+  }
+
+  private loadResolvers(dataSource: BaseDataSource): void {
+    const fileFilter = /([_A-Za-z][_0-9A-Za-z]*)\.([_A-Za-z][_0-9A-Za-z]*)\.\[(\w+)\]\.[ts|js]/
+    const dirFilter = /([_A-Za-z][_0-9A-Za-z]*)\.([_A-Za-z][_0-9A-Za-z]*)/
+
+    this.resolverEntries.forEach((entry) => {
+      if (entry.isFile()) {
+        const match = entry.name.match(fileFilter)
+        if (match) {
+          const [, typeName, fieldName, dataSourceName] = match
+          if (dataSourceName === dataSource.name) {
+            const resolver = dataSource.createJsResolver(typeName, fieldName, {
+              resolverFile: path.join(entry.path, entry.name),
+            })
+            this.unitResolvers[`${typeName}.${fieldName}`] = resolver
+          }
+        }
+      } else if (entry.isDirectory()) {
+        const match = entry.name.match(dirFilter)
+        if (match) {
+          const [, typeName, fieldName] = match
+          this.loadPipelineResolver(
+            typeName,
+            fieldName,
+            dataSource,
+            path.join(entry.path, entry.name)
+          )
+        }
+      }
+    })
+  }
+
+  private loadPipelineResolver(
+    typeName: string,
+    fieldName: string,
+    dataSource: BaseDataSource,
+    folder: string
+  ) {
+    const fileFilter = /(resolver)\.[ts|js]/
+
+    let resolver: Resolver
+    let conf = this.pipelineResolvers[`${typeName}.${fieldName}`]
+
+    if (conf) {
+      resolver = conf.resolver
+    } else {
+      const file = this.resolverEntries.filter(
+        (ent) =>
+          ent.isFile() &&
+          ent.path.endsWith(`${typeName}.${fieldName}`) &&
+          ent.name.match(fileFilter)
+      )
+
+      if (file.length > 0) {
+        resolver = this.createJsPipelineResolver(typeName, fieldName, {
+          resolverFile: path.join(folder, file[0].name),
+        })
+      } else {
+        resolver = new Resolver(this, `${typeName}_${fieldName}_resolver`, {
+          api: this,
+          dataSource,
+          typeName,
+          fieldName,
+          runtime: FunctionRuntime.JS_1_0_0,
+          code: Code.fromInline(DEFAULT_PIPELINE_RESOLVER_CODE),
+        })
+      }
+      conf = { resolver, fns: [] }
+    }
+
+    const fns = this.loadFunctions(typeName, fieldName, dataSource, folder)
+
+    this.pipelineResolvers[`${typeName}.${fieldName}`] = {
+      resolver: conf.resolver,
+      fns: conf.fns.concat(fns).sort((a, b) => a.order - b.order),
+    }
+    conf = this.pipelineResolvers[`${typeName}.${fieldName}`]
+
+    const node = resolver.node.defaultChild as CfnResolver
+    node.kind = 'PIPELINE'
+    node.pipelineConfig = {
+      functions: conf.fns.map((fn) => fn.fn.functionId),
+    }
+  }
+
+  private loadFunctions(
+    typeName: string,
+    fieldName: string,
+    dataSource: BaseDataSource,
+    folder: string
+  ) {
+    const fileFilter = /(\d+)\.([_A-Za-z][_0-9A-Za-z]*)\.\[(\w+)\]\.[ts|js]/
+
+    const entries = this.resolverEntries.filter(
+      (ent) => ent.isFile() && ent.path.endsWith(`${typeName}.${fieldName}`)
+    )
+
+    const fns: { order: number; fn: AppsyncFunction }[] = []
+    entries.forEach((entry) => {
+      const match = entry.name.match(fileFilter)
+      if (match) {
+        const [, order, name, dataSourceName] = match
+        if (dataSourceName === dataSource.name) {
+          const fn = dataSource.createJsFunction(
+            `${typeName}_${fieldName}_${dataSourceName}_${name}`,
+            {
+              functionFile: path.join(entry.path, entry.name),
+            }
+          )
+          fns.push({ order: parseInt(order), fn })
+        }
+      }
+    })
+    return fns.sort((a, b) => a.order - b.order)
   }
 }
---
+++ lib/utils.ts
@@ -0,0 +1,206 @@
+import { spawnSync, SpawnSyncOptions } from 'child_process'
+import * as fs from 'fs'
+import * as os from 'os'
+import * as path from 'path'
+// import { Code } from './code';
+// import { AppSyncBundlingOptions } from './types';
+// import { AssetHashType, BundlingOutput, DockerImage, FileSystem } from '../../core';
+
+interface CallSite {
+  getThis(): any
+  getTypeName(): string
+  getFunctionName(): string
+  getMethodName(): string
+  getFileName(): string
+  getLineNumber(): number
+  getColumnNumber(): number
+  getFunction(): Function
+  getEvalOrigin(): string
+  isNative(): boolean
+  isToplevel(): boolean
+  isEval(): boolean
+  isConstructor(): boolean
+}
+
+/**
+ * Get callsites from the V8 stack trace API
+ *
+ * https://github.com/sindresorhus/callsites
+ */
+function callsites(): CallSite[] {
+  const _prepareStackTrace = Error.prepareStackTrace
+  Error.prepareStackTrace = (_, stack) => stack
+  const stack = new Error().stack?.slice(1)
+  Error.prepareStackTrace = _prepareStackTrace
+  return stack as unknown as CallSite[]
+}
+
+/**
+ * Spawn sync with error handling
+ */
+function exec(cmd: string, args: string[], options?: SpawnSyncOptions) {
+  const proc = spawnSync(cmd, args, options)
+
+  if (proc.error) {
+    throw proc.error
+  }
+
+  if (proc.status !== 0) {
+    if (proc.stdout || proc.stderr) {
+      throw new Error(
+        `[Status ${proc.status}] stdout: ${proc.stdout
+          ?.toString()
+          .trim()}\n\n\nstderr: ${proc.stderr?.toString().trim()}`
+      )
+    }
+    throw new Error(
+      `${cmd} ${args.join(' ')} ${
+        options?.cwd ? `run in directory ${options.cwd}` : ''
+      } exited with status ${proc.status}`
+    )
+  }
+
+  return proc
+}
+
+/**
+ * Finds the name of the file where the AppSync JS resource is defined
+ */
+export function findDefiningFile(functionName: string): string {
+  let definingIndex
+  const sites = callsites()
+  for (const [index, site] of sites.entries()) {
+    console.log(site, site.getFunctionName())
+    if (site.getFunctionName() === functionName) {
+      definingIndex = index + 1
+      break
+    }
+  }
+
+  if (!definingIndex || !sites[definingIndex]) {
+    throw new Error('Cannot find defining file.')
+  }
+
+  return sites[definingIndex].getFileName()
+}
+
+export function getResolverName(typeName: string, fieldName: string) {
+  return `${typeName}${fieldName[0].toUpperCase() + fieldName.slice(1)}Resolver`
+}
+
+/**
+ * Searches for a resolver file. Preference order is the following:
+ * 1. Given `${resolverFile}`
+ * 2. A file named `${resolverDir}/${typeName}.{fieldName}.[ts|js]`
+ * 3. A file named `resolvers/${typeName}.{fieldName}.[ts|js]` in the directory of the defining file.
+ */
+export function findResolverEntry(
+  typeName: string,
+  fieldName: string,
+  resolverFile?: string,
+  resolverDir?: string,
+  isPipelineResolver?: boolean
+): string {
+  if (resolverFile) {
+    if (!/\.(js|ts)$/.test(resolverFile)) {
+      throw new Error('Only JavaScript or TypeScript files are supported.')
+    }
+    if (!fs.existsSync(resolverFile)) {
+      throw new Error(`Cannot find resolver file at ${resolverFile}`)
+    }
+    return resolverFile
+  }
+
+  const dirname =
+    resolverDir ??
+    path.join(
+      path.dirname(
+        findDefiningFile(isPipelineResolver ? 'createJsPipelineResolver' : 'createJsResolver')
+      ),
+      'resolvers'
+    )
+
+  const tsFile = path.join(dirname, `${typeName}.${fieldName}.ts`)
+  if (fs.existsSync(tsFile)) {
+    return tsFile
+  }
+
+  const jsFile = path.join(dirname, `${typeName}.${fieldName}.js`)
+  if (fs.existsSync(jsFile)) {
+    return jsFile
+  }
+
+  throw new Error(`Cannot find resolver file ${tsFile}, or ${jsFile}.`)
+}
+
+type ResolverEntry = {
+  typeName: string
+  fieldName: string
+  entryFile: string
+}
+export function findResolverEntries(resolverDir: string[], defaultDir: boolean) {
+  const dirname = defaultDir
+    ? path.join(path.dirname(findDefiningFile('loadJsResolvers')), ...resolverDir)
+    : resolverDir[0]
+
+  //list all files in the `dirname` folder
+  const resolverFiles = fs.readdirSync(dirname).map((file) => {
+    const match = file.match(/(\w+)\.(\w+)\.(js|ts)/)
+    if (match) {
+      return {
+        typeName: match[1],
+        fieldName: match[2],
+        entryFile: path.join(dirname, file),
+      }
+    }
+    return null
+  })
+  return resolverFiles.filter((f) => f) as ResolverEntry[]
+}
+
+/**
+ * Searches for function file. Preference order is the following:
+ * 1. Given `${functionFile}`
+ * 2. A file named `${functionDir}/${name}.[ts|js]`
+ * 3. A file named `functions/${name}.[ts|js]` in the directory of the defining file.
+ * Note: `name` can provide the file extension.
+ */
+export function findFunctionEntry(
+  name: string,
+  functionFile?: string,
+  functionDir?: string
+): string {
+  if (functionFile) {
+    if (!/\.(js|ts)$/.test(functionFile)) {
+      throw new Error('Only JavaScript or TypeScript files are supported.')
+    }
+    if (!fs.existsSync(functionFile)) {
+      throw new Error(`Cannot find function file at ${functionFile}`)
+    }
+    return functionFile
+  }
+
+  const dirname =
+    functionDir ??
+    path.join(path.dirname(findDefiningFile('createJsPipelineResolver')), 'functions')
+
+  let needsExtension = true
+  if (/\.\w+$/.test(name)) {
+    if (!/\.(js|ts)$/.test(name)) {
+      throw new Error('Only JavaScript or TypeScript files are supported.')
+    }
+    needsExtension = false
+  }
+
+  const tsFile = path.join(dirname, `${name}${needsExtension ? '.ts' : ''}`)
+  if (fs.existsSync(tsFile)) {
+    return tsFile
+  }
+
+  const jsFile = path.join(dirname, `${name}${needsExtension ? '.js' : ''}`)
+  if (fs.existsSync(jsFile)) {
+    return jsFile
+  }
+
+  throw new Error(`Cannot find function file ${tsFile}, or ${jsFile}.`)
+}
